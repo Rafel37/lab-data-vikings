d8c69a9ab89e49119393b9ba7e15872082a6349a
Rafel37 <rafelperez37@hotmail.es>
2019-06-01 13:38:39 +0200

    dos saxon menos de los que preocuparse

--- lab_vikings/vikings_clases.py
+++ lab_vikings/vikings_clases.py
@@ -41,7 +41,7 @@ class Saxon(Soldier):
         if self.health > 0:
             return "A Saxon has received " + str(damage) + " points of damage"
         else:
-            return "A Saxon has died in act of combat"
+            return "A Saxon has died in combat"
 
 # War
 class War:
